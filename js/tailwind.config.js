38aa2a169930d9f5ecf5e20b3ec996802452831c
TkMatE <tkmate2234@gmail.com>
2026-01-17 14:32:35 +0300

    Add main website structure and contact page
    - Created `index.html` for the homepage with a hero section, services preview, and rental box pricing.
    - Added `iletisim.html` for the contact page, including a contact form and contact details.
    - Introduced `tailwind.config.js` for custom Tailwind CSS configurations.
    - Added an image `index.jpeg` for the homepage background.

---
+++ js/tailwind.config.js
@@ -0,0 +1,21 @@
+tailwind.config = {
+    darkMode: "class",
+    theme: {
+        extend: {
+            colors: {
+                "primary": "#1f3f65",
+                "background-light": "#f6f7f8",
+                "background-dark": "#14191e",
+            },
+            fontFamily: {
+                "display": ["Manrope", "sans-serif"]
+            },
+            borderRadius: {
+                "DEFAULT": "0.5rem",
+                "lg": "1rem",
+                "xl": "1.5rem",
+                "full": "9999px"
+            },
+        },
+    },
+}
